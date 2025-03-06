04ccee0f718dee260447380538567ac1f8eb4b62
Evgen <genitya408@gmail.com>
2025-03-06 22:33:00 +0300

    initial

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:chat_app_dinopr/themes/light_mode.dart';
+import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+      theme: lightMode,
+    );
+  }
+}
+
