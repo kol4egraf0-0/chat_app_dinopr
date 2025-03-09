dfa6651cd0c326a5ae453dd013ede00df90dd7bc
Evgen <genitya408@gmail.com>
2025-03-09 22:21:38 +0300

    chat service and message model

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,8 +1,11 @@
+import 'package:chat_app_dinopr/models/message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatService {
-  //инициал бд frb
+  //инициал бд frb и auth
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   
   //id польз
   Stream<List<Map<String,dynamic>>> getUsersStream(){
@@ -17,6 +20,38 @@ class ChatService {
      
     }
   //отправить message
+  Future<void> sendMessage(String receiverID, message) async{
+    //get current user info
+    final String currentUserID = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email!;
+    final Timestamp timestamp = Timestamp.now();
 
-  //получить 
+
+    //создать новое сообщение
+    Message newMessage = Message(senderID: currentUserID, senderEmail: currentUserID, receiverID: receiverID, message: message, timestamp: timestamp);
+
+    //построить конату чата для 2ух людей
+    List<String> ids = [currentUserID, receiverID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+
+    //добавить новое сообщение в бд
+    await _firestore.collection("chat_rooms")
+    .doc(chatRoomID).collection("messages").add(newMessage.toMap());
+  }
+
+  //получить сообщение
+  Stream<QuerySnapshot> getMessages(String userID, otherUserID)
+  {
+    List<String> ids = [userID, otherUserID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+
+    return _firestore
+    .collection("chat_rooms")
+    .doc(chatRoomID)
+    .collection("messages")
+    .orderBy("timestamp", descending: false)
+    .snapshots();
+  }
 }
