5771078090b37155439576c0c36cc7f033f11969
Evgen <genitya408@gmail.com>
2025-03-10 11:58:56 +0300

    keyboard fix and good job

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -5,29 +5,69 @@ import 'package:chat_app_dinopr/services/chat/chat_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   final String receiverEmail;
   final String receiverID;
   ChatPage({super.key, required this.receiverEmail, required this.receiverID});
 
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
   final TextEditingController _messageController = TextEditingController();
 
   final ChatService _chatService = ChatService();
   final AuthService _authService = AuthService();
 
-  void sendMessage() async{
-    if(_messageController.text.isNotEmpty){
-      await _chatService.sendMessage(receiverID, _messageController.text);
+  //for textField focus
+  FocusNode myFocusNode = FocusNode();
+
+  @override
+void initState() {
+  super.initState();
+
+  // listener на фокус
+  myFocusNode.addListener(() {
+    if (myFocusNode.hasFocus) {
+      Future.delayed(
+        const Duration(milliseconds: 300), // задержка для корректного срабатывания
+        () => ScrollDown(), // Вызов функции для прокрутки вниз
+      );
     }
-    
-    //отчистить textContl
-    _messageController.clear();
+  });
+}
+
+
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    _messageController.dispose();
+    super.dispose();
   }
+
+  //scroll controller
+  final ScrollController _scrollController = ScrollController();
+  void ScrollDown(){
+    _scrollController.animateTo(_scrollController.position.maxScrollExtent, duration: const Duration(seconds: 1), curve: Curves.fastOutSlowIn,);
+  }
+
+  void sendMessage() async {
+  if (_messageController.text.isNotEmpty) {
+    await _chatService.sendMessage(widget.receiverID, _messageController.text);
+  }
+
+  _messageController.clear();
+  FocusScope.of(context).requestFocus(myFocusNode); // Оставляем фокус на поле ввода
+  ScrollDown(); // Прокрутка вниз
+}
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(title: Text(receiverEmail),
+      appBar: AppBar(title: Text(widget.receiverEmail),
       backgroundColor: Colors.transparent,
       foregroundColor: Colors.grey,
       elevation: 0,),
@@ -42,10 +82,11 @@ class ChatPage extends StatelessWidget {
       ),
     );
   }
+
   Widget _buildMessageList(){
     String senderID = _authService.getCurrentUser()!.uid;
     return StreamBuilder(
-      stream: _chatService.getMessages(receiverID, senderID), 
+      stream: _chatService.getMessages(widget.receiverID, senderID), 
     builder: (context, snapshot){
       if(snapshot.hasError){
         return const Text("Ошибка");
@@ -54,11 +95,13 @@ class ChatPage extends StatelessWidget {
         return const Text("Загрузка");
       }
       return ListView(
+        controller: _scrollController,
         children: snapshot.data!.docs.map((doc)=>_buildMessageItem(doc)).toList(),
       );
     }
     );
   }
+
   Widget _buildMessageItem(DocumentSnapshot doc){
     Map<String, dynamic> data=doc.data() as Map<String, dynamic>;
 
@@ -80,13 +123,14 @@ class ChatPage extends StatelessWidget {
         ],
       ));
   }
+
   Widget _buildUserInput(){
     return Padding(
       padding: const EdgeInsets.only(bottom: 50.0),
       child: Row(
         children: [
           Expanded(
-            child: MyTextField(hintText: "Напишите сообщение", obscureText: false, controller: _messageController)),
+            child: MyTextField(hintText: "Напишите сообщение", obscureText: false, controller: _messageController, focusNode: myFocusNode,)),
             //отправить кнопка
           Container(
             decoration: const BoxDecoration(color: Colors.green, shape: BoxShape.circle),
