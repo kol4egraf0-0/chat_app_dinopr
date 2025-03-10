f9797310a4023516e28e510dbf16cdc9776b48a2
Evgen <genitya408@gmail.com>
2025-03-10 10:57:00 +0300

    back of themeMode

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:chat_app_dinopr/services/auth/auth_gate.dart';
 import 'package:chat_app_dinopr/firebase_options.dart';
-import 'package:chat_app_dinopr/themes/light_mode.dart';
+import 'package:chat_app_dinopr/themes/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(create: (context)=>ThemeProvider(),
+    child: const MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,7 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const AuthGate(),
-      theme: lightMode,
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
